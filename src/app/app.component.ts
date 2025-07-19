a15ee30ca2eda5c9cd9ee739786ec4606caccab1
gabrif9 <gabrieleferrigno95@gmail.com>
2025-07-19 12:22:03 +0200

    feat: add route home and home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { HeaderComponent } from "./components/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
