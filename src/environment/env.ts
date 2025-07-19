1d4201b064734aea63705f2052ba23720a9b177b
gabrif9 <gabrieleferrigno95@gmail.com>
2025-07-19 12:53:02 +0200

    feat: add environment files

---
+++ src/environment/env.ts
@@ -0,0 +1,1 @@
+export const WEATHER_API_URL = "http://127.0.0.1:3658/m1/939077-922220-default"
---
+++ src/environment/environment.ts
@@ -0,0 +1,2 @@
+export const CURRENT_WEATHER_URL = "/data/2.5/weather"
+export const FIVE_DAYS_WEATHER_URL = "/data/2.5/forecast"
