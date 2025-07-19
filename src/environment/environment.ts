f639dbee9ab1a2d64edfd3999ded8452cbcf9577
gabrif9 <gabrieleferrigno95@gmail.com>
2025-07-19 15:25:48 +0200

    feat: add cities server environment var

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,6 @@
-import { WEATHER_API_URL } from "./env"
+import { CITY_API_URL, WEATHER_API_URL } from "./env"
 
 export const CURRENT_WEATHER_URL = WEATHER_API_URL + "/data/2.5/weather"
 export const FIVE_DAYS_WEATHER_URL = WEATHER_API_URL + "/data/2.5/forecast"
+
+export const CITY_ENDPOINT = CITY_API_URL + "/cityname"
