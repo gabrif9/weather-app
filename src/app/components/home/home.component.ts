2071db680283194c812af27205def94ebf0453ec
gabrif9 <gabrieleferrigno95@gmail.com>
2025-07-19 14:11:40 +0200

    feat: add searchLocation in home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { SearchLocationComponent } from "../search-location/search-location.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SearchLocationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
