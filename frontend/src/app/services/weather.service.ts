b368b445df6ae101f1be5ec8e6af3b1da88b468e
gabrif9 <gabrieleferrigno95@gmail.com>
2025-07-20 16:17:20 +0200

    fix: change APPID to api_key

--- frontend/src/app/services/weather.service.ts
+++ frontend/src/app/services/weather.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { CurrentWeather } from '../models/current-weather.model';
 import { CURRENT_WEATHER_URL, FIVE_DAYS_WEATHER_URL } from '../../environment/environment';
 import { FiveDaysWeather } from '../models/five-days-weather.mode';
-import { APPID } from '../../environment/env';
+import { API_KEY } from '../../environment/env';
 
 @Injectable({
   providedIn: 'root'
@@ -16,10 +16,10 @@ export class WeatherService {
   constructor() { }
 
   getCurrentWeather(lat: number, lon: number): Observable<CurrentWeather> {
-    return this.http.get<CurrentWeather>(CURRENT_WEATHER_URL, {params: {lat: lat, lon: lon, appid: APPID, units: 'metric'}})
+    return this.http.get<CurrentWeather>(CURRENT_WEATHER_URL, {params: {lat: lat, lon: lon, APPID: API_KEY, units: 'metric'}})
   }
 
   getFiveDaysForecast(lat: number, lon: number): Observable<FiveDaysWeather> {
-    return this.http.get<FiveDaysWeather>(FIVE_DAYS_WEATHER_URL, {params: {lat: lat, lon: lon, appid: APPID, units: 'metric'}})
+    return this.http.get<FiveDaysWeather>(FIVE_DAYS_WEATHER_URL, {params: {lat: lat, lon: lon, APPID: API_KEY, units: 'metric'}})
   }
 }
