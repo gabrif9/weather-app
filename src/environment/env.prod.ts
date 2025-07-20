738c726283b82ed0078c061c32bd47e2d62a418d
gabrif9 <gabrieleferrigno95@gmail.com>
2025-07-20 10:01:31 +0200

    feat: add env.prod.ts environment file, add dockerfile and default.conf configuration for nginx

---
+++ src/environment/env.prod.ts
@@ -0,0 +1,4 @@
+export const WEATHER_API_URL = "http://api.openweathermap.org"
+export const CITY_API_URL = ""
+
+export const APPID = ""
--- src/environment/env.ts
+++ src/environment/env.ts
@@ -1,3 +1,4 @@
+// export const WEATHER_API_URL = "http://api.openweathermap.org"
 export const WEATHER_API_URL = "http://127.0.0.1:3658/m1/939077-922220-default"
 export const CITY_API_URL = "http://localhost:3000"
 
